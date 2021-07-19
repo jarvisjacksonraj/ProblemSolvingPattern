9756d8ee8a0ddb2f4eef97258ceab6ec59eb30d0
jarvisjacksonraj <53148568+jarvisjacksonraj@users.noreply.github.com>
2021-07-19 20:57:10 +0530

    Sliding Window Problem 1

--- SlidingWindowPattern/src/main/java/AverageOfSubArrayOfSizeK.java
+++ SlidingWindowPattern/src/main/java/AverageOfSubArrayOfSizeK.java
@@ -1,17 +1,35 @@
 public class AverageOfSubArrayOfSizeK {
-    public static double[] findAverages(int K, int[] arr) {
-        double[] result = new double[arr.length - K + 1];
+
+    public static double[] findAverages(int K, int[] numArr) {
+
+        double[] result = new double[numArr.length - K + 1];
         double windowSum = 0;
         int windowStart = 0;
-        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-            windowSum += arr[windowEnd];
+        for (int windowEnd = 0; windowEnd < numArr.length; windowEnd++) {
+            windowSum += numArr[windowEnd];
             if (windowEnd >= K - 1) {
                 result[windowStart] = windowSum / K;
-                windowSum -= arr[windowStart];
+                windowSum -= numArr[windowStart];
                 windowStart++;
             }
         }
 
         return result;
+    }
+
+    public static double findMaxAverage(int k, int[] numArr) {
+
+        int windowStart = 0;
+        double windowSum = 0;
+        double result = Integer.MIN_VALUE;
+        for (int windowEnd = 0; windowEnd < numArr.length; windowEnd++) {
+            windowSum += numArr[windowEnd];
+            if (windowEnd >= k - 1) {
+                result = (result > windowSum) ? result : windowSum;
+                windowSum -= numArr[windowStart++];
+            }
+        }
+
+        return result / k;
     }
 }
--- SlidingWindowPattern/src/test/java/TestAverageOfSubArrayOfSizeK.java
+++ SlidingWindowPattern/src/test/java/TestAverageOfSubArrayOfSizeK.java
@@ -1,13 +1,20 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestAverageOfSubArrayOfSizeK {
 
     @Test
-    public void test1() {
+    public void TestFindAverages() {
         double[] result = AverageOfSubArrayOfSizeK.findAverages(5, new int[]{1, 3, 2, 6, -1, 4, 1, 8, 2});
         assertArrayEquals(result, new double[]{2.2, 2.8, 2.4, 3.6, 2.8});
+    }
+
+    @Test
+    public void TestFindMaxAverage() {
+        double result = AverageOfSubArrayOfSizeK.findMaxAverage(5, new int[]{1, 3, 2, 6, -1, 4, 1, 8, 2});
+        assertEquals(result, 3.6);
     }
 
 }
