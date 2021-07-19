e4bedad1e6461745577bcac7e16ffe6a9b834446
jarvisjacksonraj <53148568+jarvisjacksonraj@users.noreply.github.com>
2021-07-19 16:50:02 +0530

    Intial Setup for Problem Solving Pattern

---
+++ SlidingWindowPattern/src/main/java/AverageOfSubArrayOfSizeK.java
@@ -0,0 +1,6 @@
+public class AverageOfSubArrayOfSizeK {
+    public static double[] findAverages(int K, int[] arr) {
+        double[] result = new double[0];
+        return result;
+    }
+}
---
+++ SlidingWindowPattern/src/test/java/TestAverageOfSubArrayOfSizeK.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class TestAverageOfSubArrayOfSizeK {
+
+    @Test
+    public void test1() {
+        double[] result = AverageOfSubArrayOfSizeK.findAverages(5, new int[]{1, 3, 2, 6, -1, 4, 1, 8, 2});
+    }
+
+}
